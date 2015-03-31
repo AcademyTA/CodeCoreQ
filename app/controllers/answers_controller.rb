e229b24e23766ffd503da017f94ef49bfb11a3fd
Jason <cameljeet@gmail.com>
2015-03-31 15:17:17 -0700

    Refactored code in Answers Controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,20 +1,15 @@
 class AnswersController < ApplicationController
   # before_action :authenticate_user!
-  # before_action :find_question, only: [:update] 
+  before_action :find_question
 
   def new
-    # render text: params
-    @question = Question.find params[:question_id]
   end
 
   def index
-    @question = Question.find params[:question_id]
-    @answers = @question.answers
+    @answers = @question.answers #grabs all the answers from the question
   end
 
   def create
-    # render text: params
-    @question = Question.find params[:question_id]
     @answer = Answer.new answer_params
     @answer.question_id = @question.id
     if @answer.save
@@ -25,13 +20,13 @@ class AnswersController < ApplicationController
   end
 
   def edit
-    @question = Question.find params[:question_id]
+    # @question = Question.find params[:question_id]
     @answer = Answer.find(params[:id])
     # render text: params
   end
 
   def update
-    @question = Question.find params[:question_id]
+    # @question = Question.find params[:question_id]
     # @answer = @question.answers.find(params[:id])
     @answer = Answer.find(params[:id])
     if @answer.update answer_params
@@ -42,21 +37,22 @@ class AnswersController < ApplicationController
   end
 
   def destroy
-    @question = Question.find params[:question_id]
     @answer = @question.answers.find(params[:id])
     if @answer.destroy
-      redirect_to quiz_questions_path(@question.quiz), notice: "Answer Deleted."
+      redirect_to question_answers_path(@question), notice: "Answer Deleted."
     else
-      redirect_to quiz_questions_path(@question.quiz), alert: "Answer FAILED Delete."
+      redirect_to question_answers_path(@question), alert: "Answer FAILED Delete."
     end
   end
 
   private
 
   def find_question
-    @question = Quiz.find(params[:question_id])
+    @question = Question.find(params[:question_id])
   end
+
   def answer_params
     params.require(:answer).permit(:body, :correct, :question_id)
   end
+
 end
