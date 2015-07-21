9e9921e88cd13298a4804187a1a530dfb51eff14
Jason <cameljeet@gmail.com>
2015-07-20 22:32:56 -0700

    Rspec Answer Model validations continued

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -11,5 +11,17 @@ RSpec.describe Answer, type: :model do
       answer = Answer.new(valid_attributes({body: nil}))
       expect(answer).to be_invalid
     end
+    it "requires correct to be a boolean" do
+      answer = Answer.new(valid_attributes({correct: nil}))
+      expect(answer).to be_invalid
+    end
+    it "accepts true as a value for correct" do
+      answer = Answer.new(valid_attributes({correct: true}))
+      expect(answer).to be_valid
+    end
+    it "accepts false as a value for correct" do
+      answer = Answer.new(valid_attributes({correct: false}))
+      expect(answer).to be_valid
+    end
   end
 end
