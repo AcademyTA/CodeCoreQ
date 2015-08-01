61f59f345b34c71d451ea2064aa952b15d3d0ab7
Jason <cameljeet@gmail.com>
2015-08-01 15:51:03 -0700

    Add test for users#new variable

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-
+  describe "#new" do
+    it "instantiates a new user variable" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
 
 end
