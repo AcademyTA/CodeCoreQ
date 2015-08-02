9347df66b4a10e6107923ded5cc5f4bdaa397409
Jason <cameljeet@gmail.com>
2015-08-02 15:56:03 -0700

    Finish tests for users#new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe UsersController, type: :controller do
       get :new
       expect(assigns(:user)).to be_a_new(User)
     end
+
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
   end
 
 end
