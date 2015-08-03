df41d37496788e7739dad4b69bd330cf7f835f0b
Jason <cameljeet@gmail.com>
2015-08-03 16:21:46 +0530

    Added users index

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user)   { create(:user) }
+  let(:user_1) { create(:user_1) }
 
   describe "#new" do
     it "instantiates a new user variable" do
@@ -11,6 +13,13 @@ RSpec.describe UsersController, type: :controller do
     it "renders the new template" do
       get :new
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe "#index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
     end
   end
 
