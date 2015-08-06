6e9a307cce3c73954151b1b7da09415b408499e5
Jason <cameljeet@gmail.com>
2015-08-05 20:59:12 -0800

    Add test for users#create

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,25 +1,47 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
-  let(:user)   { create(:user) }
-  let(:user_1) { create(:user_1) }
-
-  describe "#new" do
-    it "instantiates a new user variable" do
+  describe '#new' do
+    it 'instantiates a new user variable' do
       get :new
       expect(assigns(:user)).to be_a_new(User)
     end
 
-    it "renders the new template" do
+    it 'renders the new template' do
       get :new
       expect(response).to render_template(:new)
     end
   end
 
-  describe "#index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template(:index)
+  describe "#create" do
+    context "with valid parameters" do
+      def valid_request
+        post :create, 
+          { 
+            user: {
+              name: "John Derpinter",
+              email: "jderp@codecore.ca",
+              password: "abcd1234",
+              password_confirmation: "abcd1234"
+            }
+          }
+      end
+
+      it "creates a user in the database" do
+        expect { valid_request }.to change { User.count }.by(1)
+      end
+
+      it "sets a flash message" do
+        valid_request
+        expect(flash[:success]).to be
+      end
+
+      it "redirects to the root path of the application" do
+        valid_request
+        user = User.last
+        expect(response).to redirect_to(user)
+      end
     end
   end
 
