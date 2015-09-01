226921b12f691a50e19d793aabc6ea79c3b4df1a
Jason <cameljeet@gmail.com>
2015-09-01 00:02:45 -0800

    Reformat code in quizzes controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,14 +2,15 @@ class QuizzesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def new
-    @quiz = Quiz.new
+    @quiz       = Quiz.new
     @categories = Category.all
   end
 
   def create
     # render text: params
+    @quiz       = Quiz.new(quiz_params)
     @categories = Category.all
-    @quiz = Quiz.new quiz_params
+
     if @quiz.save
       flash[:success] = "Quiz Created!"
       redirect_to @quiz
@@ -20,32 +21,30 @@ class QuizzesController < ApplicationController
   end
 
   def show
-    @quiz = Quiz.find(params[:id])
-    @category = Category.find(@quiz.category_id)
-    @questions = @quiz.questions
-
+    @quiz         = Quiz.find(params[:id])
+    @category     = Category.find(@quiz.category_id)
+    @questions    = @quiz.questions
     new_user_quiz = true
+
     UserQuiz.all.each do |uq|
       if uq.quiz == @quiz && uq.user == current_user
         new_user_quiz = false
       end
     end
+
     if new_user_quiz
       UserQuiz.create(user_id: current_user.id, quiz_id: @quiz.id)
     end
   end
 
-
   def index
-    @quizzes = Quiz.all
+    @quizzes    = Quiz.all
     @categories = Category.all
   end
 
-
-
   def edit
+    @quiz       = Quiz.find(params[:id])
     @categories = Category.all
-    @quiz = Quiz.find(params[:id])
   end
 
   def update
@@ -63,14 +62,12 @@ class QuizzesController < ApplicationController
     @quiz = Quiz.find (params[:id])
     @quiz.destroy
     redirect_to quizzes_path
-
   end
 
-
   private
+
   def quiz_params 
     params.require(:quiz).permit(:title, :body, :level, :category_id)
   end
-
 
 end
