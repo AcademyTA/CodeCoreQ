4276e6ae9c3529da8bd3e74e4620200800767f6d
Jason <cameljeet@gmail.com>
2015-12-26 11:23:55 -0800

    rspec answers#index, user signed in

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe AnswersController, type: :controller do
   let(:quiz)     { create(:quiz) }
   let(:question) { create(:question, quiz: quiz) }
   let(:answer)   { create(:answer, question: question) }
+  let(:answer_1) { create(:answer, question: question) }
 
   describe "#new" do
     context "user signed in" do
@@ -25,6 +26,27 @@ RSpec.describe AnswersController, type: :controller do
 
       it "redirects to sign in page" do
         expect(response).to redirect_to login_path
+      end
+    end
+  end
+
+  describe "#index" do
+    context "user signed in" do
+      before { log_in(user) }
+      before { get :index, question_id: question.id }
+
+      it "renders the index template" do
+        expect(response).to render_template(:index)
+      end
+
+      it "set a instance variable to question" do
+        expect(assigns(:question)).to eq(question)
+      end
+
+      it "set a instance variable to for question answers" do
+        answer
+        answer_1
+        expect(assigns(:answers)).to eq([answer, answer_1])
       end
     end
   end
